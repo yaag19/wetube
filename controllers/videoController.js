ed1d8876d88b40e527e7edc84cee05e14f9bc217
yaag19 <m.sun.1290@gmail.com>
2020-03-28 19:50:35 +0900

    비디오 delete기능 구현

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -74,7 +74,7 @@ export const videoDetail = async (req, res) => {
     try {
         const video = await Video.findById(id);
         res.render("videoDetail", {
-            pageTitle: "Video Detail",
+            pageTitle: video.title,
             video
         });
     } catch (error) {
@@ -125,6 +125,16 @@ export const postEditVideo = async (req, res) => {
     }
 };
 
-export const deleteVideo = (req, res) => res.render("deleteVideo", {
-    pageTitle: "Delete Video"
-});
+export const deleteVideo = async (req, res) => {
+    const {
+        params: {
+            id
+        }
+    } = req;
+    try {
+        await Video.findOneAndRemove({
+            _id: id
+        });
+    } catch (error) {}
+    res.redirect(routes.home);
+};
