204de94724d28aa519041bd7361316b045ed9d1a
yaag19 <m.sun.1290@gmail.com>
2020-03-30 23:08:03 +0900

    eslint 설치

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
-import {
-    localsMiddleware
-} from "./middlewares";
+import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -19,17 +17,14 @@ app.set("view engine", "pug"); //pug 세팅, pug는 express의 view engine, view
 app.use("/uploads", express.static("uploads"));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 //local변수를 global변수로 사용하도록 만들어 주는 미들웨어
 app.use(localsMiddleware);
 
-//라우터 사용
+//라우터 사용.
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
-
 
 export default app;
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -2,139 +2,94 @@ import routes from "../routes";
 import Video from "../models/Video";
 
 export const videoHome = async (req, res) => {
-    try {
-        //await부분이 끝나기 전까지는 render부분을 실행하지 않음
-        //단, 성공이나 실패여부를 따지지 않음(끝났다는게 중요)
-        const videos = await Video.find({});
-        res.render("home", {
-            pageTitle: "Home",
-            videos
-        });
-    } catch (error) {
-        console.log(error);
-        res.render("home", {
-            pageTitle: "Home",
-            videos: []
-        });
-    }
-
+  try {
+    //await부분이 끝나기 전까지는 render부분을 실행하지 않음
+    //단, 성공이나 실패여부를 따지지 않음(끝났다는게 중요)
+    const videos = await Video.find({}).sort({ _id: -1 });
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
 };
-
 
 export const searchVideo = (req, res) => {
-    const {
-        query: {
-            term: searchingBy
-        }
-    } = req;
-    res.render("search", {
-        pageTitle: "Search",
-        searchingBy,
-        videos
-    });
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy });
 };
-
-
 
 //export const videos = (req, res) => res.render("videos");
 export const getUpload = (req, res) =>
-    res.render("upload", {
-        pageTitle: "Upload"
-    });
+  res.render("upload", { pageTitle: "Upload" });
 
 export const postUpload = async (req, res) => {
-    const {
-        body: {
-            title,
-            description
-        },
-        file: {
-            path
-        }
-    } = req;
+  const {
+    body: { title, description },
+    file: { path }
+  } = req;
 
-    const newVideo = await Video.create({
-        fileUrl: path,
-        title,
-        description,
-        //path: path.replace(/\\/g, "/")
-    });
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+    //path: path.replace(/\\/g, "/")
+  });
 
-    console.log(newVideo);
-    res.redirect(routes.videoDetail(newVideo.id));
-}
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
+};
 
 export const videoDetail = async (req, res) => {
-    //req.params.id
-    const {
-        params: {
-            id
-        }
-    } = req;
-    try {
-        const video = await Video.findById(id);
-        res.render("videoDetail", {
-            pageTitle: video.title,
-            video
-        });
-    } catch (error) {
-        console.log(error);
-        res.redirect(routes.home);
-    }
-}
+  //req.params.id
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: video.title, video });
+  } catch (error) {
+    console.log(error);
+    res.redirect(routes.home);
+  }
+};
 
 //템플릿을 랜더링하는 역할
 export const getEditVideo = async (req, res) => {
-    const {
-        params: {
-            id
-        }
-    } = req;
-    try {
-        const video = await Video.findById(id);
-        res.render("editVideo", {
-            pageTitle: `Edit ${video.title}`,
-            video
-        });
-    } catch (error) {
-        res.redirect(routes.home);
-    }
-}
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 //업데이트하고 redirect하는 역할
 export const postEditVideo = async (req, res) => {
-    const {
-        params: {
-            id
-        },
-        body: {
-            title,
-            description
-        }
-    } = req;
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
 
-    try {
-        await Video.findOneAndUpdate({
-            _id: id
-        }, {
-            title,
-            description
-        });
-        res.redirect(routes.videoDetail(id));
-    } catch (error) {
-        res.redirect(routes.home);
-    }
+  try {
+    await Video.findOneAndUpdate({ _id: id }, { title, description });
+    res.redirect(routes.videoDetail(id));
+  } catch (error) {
+    res.redirect(routes.home);
+  }
 };
 
 export const deleteVideo = async (req, res) => {
-    const {
-        params: {
-            id
-        }
-    } = req;
-    try {
-        await Video.findOneAndRemove({
-            _id: id
-        });
-    } catch (error) {}
-    res.redirect(routes.home);
+  const {
+    params: { id }
+  } = req;
+  try {
+    await Video.findOneAndRemove({ _id: id });
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect(routes.home);
 };
