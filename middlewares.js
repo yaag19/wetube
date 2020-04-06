71c03c6319bdf3acd238c21c2752a534b980ffb7
yaag19 <m.sun.1290@gmail.com>
2020-04-06 22:37:03 +0900

    SCSS: Footer, Login, Join 적용

--- middlewares.js
+++ middlewares.js
@@ -2,20 +2,20 @@ import multer from "multer";
 import routes from "./routes";
 //multer : file을 업로드하면 url을 반환하는 미들웨어
 const multerVideo = multer({
-    // "/uploads/videos/" 라고 쓰면 컴퓨터의 root에 uploads폴더를 만들게 됨!
-    dest: "uploads/videos/"
+  // "/uploads/videos/" 라고 쓰면 컴퓨터의 root에 uploads폴더를 만들게 됨!
+  dest: "uploads/videos/",
 });
 
 export const localsMiddleware = (req, res, next) => {
-    //locals : 지역변수를 템플릿, 컨트롤러 등에 전역적으로 사용할 수 있게 해 줌.?
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
+  //locals : 지역변수를 템플릿, 컨트롤러 등에 전역적으로 사용할 수 있게 해 줌.?
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
 
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1,
+  };
+  next();
 };
 
 //single : 하나의 파일만 업로드 할 수 있다.
